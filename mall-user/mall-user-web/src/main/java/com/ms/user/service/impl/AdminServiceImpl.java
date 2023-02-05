a08d1b5b82a308cbae4afd87c654118d7ff0d6e9
h_moses <709820314@qq.com>
2023-02-05 19:16:26 +0800

    resolve swagger configuration

--- mall-user/mall-user-web/src/main/java/com/ms/user/service/impl/AdminServiceImpl.java
+++ mall-user/mall-user-web/src/main/java/com/ms/user/service/impl/AdminServiceImpl.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.ms.user.entity.Admin;
 import com.ms.user.mapper.AdminMapper;
 import com.ms.user.service.AdminService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AdminServiceImpl extends ServiceImpl<AdminMapper, Admin> implements AdminService {
 }
--- mall-user/mall-user-web/src/main/java/com/ms/user/service/impl/UserServiceImpl.java
+++ mall-user/mall-user-web/src/main/java/com/ms/user/service/impl/UserServiceImpl.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.ms.user.entity.User;
 import com.ms.user.mapper.UserMapper;
 import com.ms.user.service.UserService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 }
